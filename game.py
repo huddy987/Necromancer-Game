17a83e9c0961a772a12b946710eeaff27056cef9
LaneMcMartin <lmcmarti@ualberta.ca>
2018-11-17 12:31:28 -0700

    Initial commit

---
+++ game.py
@@ -0,0 +1,18 @@
+import pygame
+
+def main():
+
+        # Initialize Pygame
+        pygame.init()
+        pygame.display.set_caption("Necromancy")
+        screen = pygame.display.set_mode((800,600))
+
+        # Define the gameplay loop
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+if __name__=="__main__":
+    main()
