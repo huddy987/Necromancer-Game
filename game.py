1366b7ca58df5b598b86351be5e994c17444d982
huddy987 <huddy987@gmail.com>
2018-11-17 13:54:37 -0700

    fix edge collision

--- game.py
+++ game.py
@@ -23,35 +23,55 @@ def main():
         size_y = 10
         velocity_x = 0
         velocity_y = 0
+        w_flag = 0
+        a_flag = 0
+        s_flag = 0
+        d_flag = 0
         while running:
-            rectangle = pygame.draw.rect(screen, colors.green, [position_x,position_y, size_x, size_y])
+            rect = pygame.draw.rect(screen, colors.green, [position_x,position_y, size_x, size_y])
             pygame.display.update()
             screen.fill(colors.black)
             position_x += velocity_x
             position_y += velocity_y
 
+            # Edge collision
+
+            # D case
+            if (position_x + size_x >= width):
+                if velocity_x > 1:
+                    velocity_x = 0
+                    d_flag = 1
+            # A case
+            elif (position_x <= 0):
+                if velocity_x < 1:
+                    velocity_x = 0
+                    a_flag = 1
+            # S case
+            if (position_y + size_y >= height):
+                if velocity_y > 1:
+                    velocity_y = 0
+                    s_flag = 1
+            #W case
+            elif (position_y <= 0):
+                if velocity_y < 1:
+                    velocity_y = 0
+                    w_flag = 1
+
+            print(s_flag)
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
-                        if (position_y <= 0):
-                            velocity_y = 0
-                        else:
-                            velocity_y = -1
-                    if event.key == pygame.K_s:
-                        if (position_y >= height):
-                            velocity_y = 0
-                        else:
-                            velocity_y = 1
-                    if event.key == pygame.K_a:
-                        if (position_x <= 0):
-                            velocity_x = 0
-                        else:
-                            velocity_x = -1
-                    if event.key == pygame.K_d:
-                        if (position_x >= height):
-                            velocity_x = 0
-                        else:
-                            velocity_x = 1
+                    if event.key == pygame.K_w and w_flag == 0:
+                        velocity_y = -5
+                        s_flag = 0
+                    if event.key == pygame.K_s and s_flag == 0:
+                        velocity_y = 5
+                        w_flag = 0
+                    if event.key == pygame.K_a and a_flag == 0:
+                        velocity_x = -5
+                        d_flag = 0
+                    if event.key == pygame.K_d and d_flag == 0:
+                        velocity_x = 5
+                        a_flag = 0
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_w:
                         velocity_y = 0
