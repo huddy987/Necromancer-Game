283a21ca22816e9bb14ed94c90309961b9c4c3ee
huddy987 <huddy987@gmail.com>
2018-11-18 01:26:34 -0700

    stuff

--- enemy.py
+++ enemy.py
@@ -8,13 +8,14 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self, x, y, enemyType, enemySpeed, enemySize):
         pygame.sprite.Sprite.__init__(self)
         # Initizalize enemy to be size of global ENEMY_SIZE
-        self.image = pygame.Surface((enemySize, enemySize))
-        self.image.fill(colors.red)
+        self.images = [pygame.image.load("enemy_images/enemy0000.png"), pygame.image.load("enemy_images/enemy0001.png"), pygame.image.load("enemy_images/enemy0002.png")]
+        self.image = self.images[0]
         self.rect = self.image.get_rect()
         self.speed = enemySpeed
         self.rect.x  = x
         self.rect.y = y
         self.enemyType = enemyType
+        self.spritetimer = 0
 
     #https://stackoverflow.com/questions/20044791/how-to-make-an-enemy-follow-the-player-in-pygame
 
@@ -34,4 +35,12 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.y += 0
 
     def update(self, WIDTH, HEIGHT, player):
+        if self.spritetimer == 90:
+            self.image = self.images[2]
+            self.spritetimer = 0
+        elif self.spritetimer == 60:
+            self.image = self.images[1]
+        elif self.spritetimer == 30:
+            self.image = self.images[0]
+        self.spritetimer += 1
         self.followPlayer(player)
