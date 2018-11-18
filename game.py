c366e64d2df41b4c5006f5dbecf393749bc12295
cmput274 <cmput274@ualberta.ca>
2018-11-17 19:19:23 -0700

    Made enemies spawn approximately every ten seconds

--- game.py
+++ game.py
@@ -3,12 +3,23 @@ import colors
 import player
 import army
 import enemy
+import random
 
-def main():
+def spawnEnemies(time, frequency):
+    if (time % frequency) == 0:
+        return True
+    else:
+        return False
+
+def main():  
     # Global variables
     WIDTH = 800
     HEIGHT = 600
     FPS = 30
+    ktime = 0
+    
+    PLAYER_SIZE = 40
+    PLAYER_SPEED = 8
 
     pygame.init()
     pygame.mixer.init()
@@ -17,30 +28,32 @@ def main():
     clock = pygame.time.Clock()
 
     all_players = pygame.sprite.Group()
-    player1 = player.Player(400, 300, 40)
-    all_enemies = pygame.sprite.Group()
-    enemy1 = enemy.Enemy(200, 200, 0, 2, 40)
-    enemy2 = enemy.Enemy(10, 10, 0, 3, 40)
+    player1 = player.Player(400, 300, PLAYER_SIZE, PLAYER_SPEED)
     all_armies = pygame.sprite.Group()
     armies = army.Army(200, 400, 40, 5)
 
-    #all_sprites = pygame.sprite.Group()
     all_players.add(player1)
-    all_enemies.add(enemy1)
-    all_enemies.add(enemy2)
     all_armies.add(armies)
+
+    all_enemies = pygame.sprite.Group()
 
     running = True
     while running:
         # keep loop running at the right speed
         clock.tick(FPS)
+        ktime += 1
 
         # Process input (events)
         for event in pygame.event.get():
             # check for closing window
             if event.type == pygame.QUIT:
                 running = False
-
+                
+        # Spawn enemies based on frequency
+        if spawnEnemies(ktime, 100):
+            e = enemy.Enemy(random.randint(0, WIDTH), random.randint(0, HEIGHT), 0, random.randint(2,5), 40)
+            all_enemies.add(e)  
+    
         # Update
         all_players.update(WIDTH, HEIGHT)
         all_enemies.update(WIDTH, HEIGHT, player1)
